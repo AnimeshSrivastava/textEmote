e06d6d34bc21b0b89cb798f1faa46a94c2e88331
AnimeshSrivastava <idisanimesh@gmail.com>
2023-09-19 18:10:14 +0530

    Creating the project template using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An NLP based text analyzer to identify the emotions of the content provided',
+    author='Animesh_Srivastava',
+    license='MIT',
+)
